f5eacbd44607e1281ce063b343348d2101099d40
Gabe <gabriel.morales1@stu.ocsb.ca>
2026-04-28 13:55:07 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,32 @@
+let tool = 0
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
-basic.forever(function () {
-    basic.pause(500)
-    basic.showIcon(IconNames.Heart)
-    basic.pause(200)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.pause(200)
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showLeds(`
+            # # # # #
+            . # . # .
+            . # # # .
+            . # . # .
+            # # # # #
+            `)
+    } else if (tool == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
